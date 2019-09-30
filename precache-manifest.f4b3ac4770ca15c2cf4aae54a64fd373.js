734cbbc639b7dad18580a8698ed1ae14ec6c2467
evegreen <romenbane@gmail.com>
2019-10-01 02:01:42 +0700

    0.8.1

--- precache-manifest.f4b3ac4770ca15c2cf4aae54a64fd373.js
+++ precache-manifest.f4b3ac4770ca15c2cf4aae54a64fd373.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/graph-editor/static/js/runtime~main.0ae2c646.js"
   },
   {
-    "revision": "d0f8185c8b8ae7ea1b20",
-    "url": "/graph-editor/static/js/main.e097f8f3.chunk.js"
+    "revision": "9227f70e37ab73602ef0",
+    "url": "/graph-editor/static/js/main.a7a93795.chunk.js"
   },
   {
     "revision": "f4e1f2783d28784718fe",
     "url": "/graph-editor/static/js/2.4725baca.chunk.js"
   },
   {
-    "revision": "d0f8185c8b8ae7ea1b20",
+    "revision": "9227f70e37ab73602ef0",
     "url": "/graph-editor/static/css/main.f0b83440.chunk.css"
   },
   {
-    "revision": "742402c7e1cd6e4247062b9d609ddf72",
+    "revision": "ebed8e7b6ab4a8f3d436dd9549cad0b7",
     "url": "/graph-editor/index.html"
   }
 ];
